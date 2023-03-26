5256e02fcca366b405b3249238c31b8a12ba8f26
kaido12 <kaidofalcon@gmail.com>
2023-03-26 15:13:13 +0530

    march 26

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -27,13 +27,9 @@ const updateSingleProduct = asyncHandler(async (req, res) => {
     if (req.body.title) {
       req.body.slug = slugify(req.body.title);
     }
-    const updateAProduct = await Product.findByIdAndUpdate(
-      id,
-      body,
-      {
-        new: true,
-      }
-    );
+    const updateAProduct = await Product.findByIdAndUpdate(id, body, {
+      new: true,
+    });
     res.json(updateAProduct);
   } catch (error) {
     throw new Error(error);
@@ -57,8 +53,56 @@ const getSingleProduct = asyncHandler(async (req, res) => {
 
 const getAllProduct = asyncHandler(async (req, res) => {
   try {
-    const findAllProduct = await Product.find();
-    res.json(findAllProduct);
+    //Filtering
+    const queryObject = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.map((i) => delete queryObject[i]);
+
+    let queryString = JSON.stringify(queryObject);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    let query = Product.find(JSON.parse(queryString));
+
+    //Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // limiting the fields
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // PAGINATION
+
+    // Extract the page number and limit from the query parameters
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    // Extract the page number and limit from the query parameters
+    const skip = (page - 1) * limit;
+
+    
+    // Constructing database query using the skip and limit values
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) throw new Error("This Page does not exists");
+    }    
+
+    const findProducts = await query;
+    res.json(findProducts);
   } catch (error) {
     throw new Error(error);
   }
